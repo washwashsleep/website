dbded392ac2d10ab6f988b1c1942adfc98c5c63f
webber0928 <bird23074035@gmail.com>
2014-11-23 17:31:27 +0800

    最後沒時間了

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var db = require('../models/mongo');
 
 module.exports.index = function(req, res) {
+  if(req.session.ticket == true){
+    req.session.ticket = '資料已存入！請等候好消息！'
+  }else{
+    req.session.ticket = null
+  }
   if(req.session.faillogin == true){
     req.session.faillogin = '帳號或密碼錯誤'
   }else{
@@ -15,7 +20,7 @@ module.exports.index = function(req, res) {
   }
   if(!req.session.username)
     req.session.username = null;
-  res.render('index', { title: 'Express', user: req.session.username, faillogin: req.session.faillogin, insert:req.session.insert});
+  res.render('index', { title: 'Express', user: req.session.username, faillogin: req.session.faillogin, insert:req.session.insert, ticket: req.session.ticket});
 };
 module.exports.signin = function(req, res) {
   db.user.findOne({ pass:req.body.pass, $or:[{email:req.body.email},{name:req.body.email}] }, function (err, data){
@@ -43,11 +48,13 @@ module.exports.signup = function(req, res) {
 };
 module.exports.ticket = function(req, res) {
   db.test.insert(req.body, function (err, data){
-    if(err)
-      console.error(err)
-    console.log(data)
+    if(!data){
+      console.error(err);
+    }else{
+      req.session.ticket = true;
+    }
   })
-  res.redirect('/')
+  res.redirect('/#ticket')
 };
 module.exports.logout = function(req, res) {
   req.session.destroy();
@@ -55,5 +62,5 @@ module.exports.logout = function(req, res) {
 };
 
 function obj2string(){
-  
+
 }
